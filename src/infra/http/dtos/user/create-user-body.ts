4da490a617773228a1648d2693debdc23f0bdc65
Armando Junior <armando.junior4440@gmail.com>
2023-01-09 00:45:53 -0300

    🎨 feat: Mensagens de validação

--- src/infra/http/dtos/user/create-user-body.ts
+++ src/infra/http/dtos/user/create-user-body.ts
@@ -1,17 +1,17 @@
 import { IsEmail, IsNotEmpty, Length } from 'class-validator'
 
 export class CreateUserBody {
-  @IsNotEmpty()
-  @Length(4, 150)
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @Length(4, 150, { message: 'O nome deve ter entre 4 e 150 caracteres' })
   name: string
 
   @IsNotEmpty()
-  @IsEmail()
+  @IsEmail({}, { message: 'Email inválido' })
   email: string
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O CPF não pode ser vazio' })
   cpf: string
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'A senha não pode estar vazia' })
   password: string
 }
